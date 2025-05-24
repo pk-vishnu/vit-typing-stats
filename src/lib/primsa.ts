9d90ddeb1612df35db3a1a500533dee79d682f7c
Vishnu P K <vishnuvpk05@gmail.com>
2025-05-24 17:49:17 +0530

    chore: migrated db to postgresql for serverless deployment

--- src/lib/primsa.ts
+++ src/lib/primsa.ts
@@ -1,8 +1,15 @@
-import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
+
+/* This setup is for SQLite DEV only */
+// import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
+// import { PrismaClient } from "@prisma/client";
+
+// const adapter = new PrismaBetterSQLite3({
+//     url: "file:./prisma/dev.db",
+// });
+// const prisma = new PrismaClient({ adapter });
+// export default prisma;
+
 import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaBetterSQLite3({
-    url: "file:./prisma/dev.db",
-});
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 export default prisma;
